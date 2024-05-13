ba75ba766a7deec91702788785e227a5890711cf
gabrielthegabbo <109422906+gabrielthegabbo@users.noreply.github.com>
2024-05-13 23:49:54 +0800

    button
    get started changed to jomar pangit

--- mmsu-connect-laravel/mmsu-connect-react/src/App.jsx
+++ mmsu-connect-laravel/mmsu-connect-react/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
                             fugiat ut assumenda excepturi exercitationem quasi.
                             In deleniti eaque aut repudiandae et a id nisi.
                         </p>
-                        <button className="btn btn-primary">Get Started</button>
+                        <button className="btn btn-primary">Jomar pangitn </button>
                     </div>
                 </div>
             </div>
