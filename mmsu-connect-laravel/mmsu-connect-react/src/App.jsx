00e2ff473a9879d83637d910b139ac07df62d7aa
DevJom <jomarmlazaro@gmail.com>
2024-05-25 22:29:40 +0800

    v2.3

--- mmsu-connect-laravel/mmsu-connect-react/src/App.jsx
+++ mmsu-connect-laravel/mmsu-connect-react/src/App.jsx
@@ -3,15 +3,17 @@ import Navbar from "./navbar";
 import Home from "./pages/home";
 import Contact from "./pages/contact";
 import NoPage from "./pages/noPage";
+import Login from "./pages/login";
 
 const App = () => {
     return (
         <Routes>
+            <Route path="login" element={<Login />} />
             <Route path="/" element={<Navbar />}>
                 <Route index element={<Home />} />
-
                 <Route path="contact" element={<Contact />} />
                 <Route path="*" element={<NoPage />} />
+                
             </Route>
         </Routes>
     );
--- mmsu-connect-laravel/mmsu-connect-react/src/components/ui/hero.jsx
+++ mmsu-connect-laravel/mmsu-connect-react/src/components/ui/hero.jsx
@@ -14,9 +14,6 @@ const Hero = () => {
                     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                         <form className="card-body">
                             <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
                                 <input
                                     type="email"
                                     placeholder="email"
@@ -25,9 +22,6 @@ const Hero = () => {
                                 />
                             </div>
                             <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
                                 <input
                                     type="password"
                                     placeholder="password"
@@ -43,7 +37,7 @@ const Hero = () => {
                                     </a>
                                 </label>
                             </div>
-                            <div className="form-control mt-6">
+                            <div className="form-control mt-4">
                                 <button className="btn btn-success">
                                     Login
                                 </button>
