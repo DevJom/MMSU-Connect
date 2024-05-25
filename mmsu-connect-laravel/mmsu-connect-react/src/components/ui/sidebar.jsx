ad6f920299619e85ae03b82854a1d02670f02bdb
gabrielthegabbo <109422906+gabrielthegabbo@users.noreply.github.com>
2024-05-26 01:16:15 +0800

    v2.3.4

--- mmsu-connect-laravel/mmsu-connect-react/src/components/ui/sidebar.jsx
+++ mmsu-connect-laravel/mmsu-connect-react/src/components/ui/sidebar.jsx
@@ -72,7 +72,7 @@ const Sidebar = () => {
     }
 
     return (
-        <div className="drawer lg:drawer-open h-">
+        <div className="drawer lg:drawer-open">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
 
             <div className="drawer-side">
